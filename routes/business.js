dee40fb1b864c8bccc96ae5e137da90d62f4a130
Matt Restine <restinematt@gmail.com>
2017-05-16 15:46:22 -0500

    fixed create bug

--- routes/business.js
+++ routes/business.js
@@ -25,18 +25,18 @@ router.get('/new', middleware.isLoggedIn, function(req, res) {
 });
 // Save new business to db
 router.post('/', middleware.isLoggedIn, function(req, res) {
-    var newData = {
-        name: req.body.name,
-        logo: req.body.logo,
-        desc: req.body.desc,
-        video: req.body.video,
-        url: req.body.url,
-        owner: {
-            id:req.user._id,
+    
+        var name = req.body.name;
+        var logo = req.body.logo;
+        var desc = req.body.desc;
+        var video = req.body.video;
+        var url = req.body.url;
+        var owner = {
+            id: req.user._id,
             firstname: req.user.firstname,
             lastname: req.user.lastname
-        } 
-    };
+        };
+        var newData = {name:name, logo:logo, desc:desc, video:video, url:url, owner:owner};
     Business.create(newData, function(err, data) {
         if(err) {
             req.flash('error', err.message);
