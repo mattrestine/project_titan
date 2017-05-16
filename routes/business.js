51bb5d467e77847b24b2b9f437573a5951308676
Matt Restine <restinematt@gmail.com>
2017-05-16 15:59:11 -0500

    added name to db

--- routes/business.js
+++ routes/business.js
@@ -42,6 +42,7 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
             req.flash('error', err.message);
             res.redirect('/business');
         }
+        req.flash('success', 'Added new business: ' , req.body.name);
         res.redirect('/business');
     });
 });
