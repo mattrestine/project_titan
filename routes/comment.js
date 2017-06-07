c2897501223cdbdef82905b07e23b399636279ab
Arthur Prather <apratherjr@gmail.com>
2017-06-07 16:07:14 -0500

    Completed -- Edit and Delete Comment

--- routes/comment.js
+++ routes/comment.js
@@ -12,10 +12,10 @@ var router = express.Router({mergeParams: true});
 router.get('/new', middleware.isLoggedIn, function(req, res) {
       Business.findById(req.params.id, function(err, data) {
         if(err) {
-            console.log(err);
-            req.flash("error",  err.message);
-        }
+            req.flash('error',  err.message);
+        } else {
             res.render('comments/new', {data: data});
+        }      
     });
 });
 
@@ -24,13 +24,12 @@ router.post('/', middleware.isLoggedIn, function(req, res){
     Business.findById(req.params.id, function(err, data){
         if(err){
             console.log(err);
-            req.flash("error", err.message);
+            req.flash('error', err.message);
             res.redirect('/business');
         } else {
             Comments.create(req.body.comment, function(err, comment){
                 if(err){
-                    console.log(err);
-                    req.flash("error",  err.message);
+                    req.flash('error',  err.message);
                 } else {
                     comment.author.id = req.user._id;
                     comment.author.username = req.user.username;
@@ -39,16 +38,49 @@ router.post('/', middleware.isLoggedIn, function(req, res){
                     data.comments.push(comment);
                     data.save();
                     //--//
-                    req.flash("success", "Success: Submission Completed!");
+                    req.flash('success', "Success: Submission Completed!");
                     res.redirect('/business/' + data._id);
                 }
             });
         }
+    });
+});
+
+// Comment Get One (Edit Form)
+router.get('/:comment_id/edit', function(req, res){
+    Comments.findById(req.params.comment_id, function(err, editComment){
+        if(err){
+            req.flash('error', err.message);
+            res.redirect('back')
+        } else {
+            res.render('comments/edit', {data_id: req.params.id, comment: editComment});
+        }
+    });   
+});
+// Comment Put One
+router.put('/:comment_id', function(req, res){
+    Comments.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updateComments){
+        if(err){
+            req.flash('error', err.message);
+            res.redirect('back');
+        } else {
+            req.flash('success', "Success: Submission Updated");
+            res.redirect('/business/' + req.params.id);
+        }
+    });
+});
+
+// Comment Delete One
+router.delete('/:comment_id', function(req, res){
+    Comments.findByIdAndRemove(req.params.comment_id, req.body.comment, function(err){
+        if(err){
+            res.redirect('back');
+        } else {
+            req.flash('success', "Success: Submission Deleted");
+            res.redirect('/business/' + req.params.id);
+        }
     })
 })
 
-// Comment Get One (Edit Form)
-// Comment Put One
-// Comment Delete One
 // MODULE EXPORTS
 module.exports = router;
