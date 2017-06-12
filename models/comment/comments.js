a37e513b896e60211dc5bf6ea6c23736504a4a13
Arthur Prather <apratherjr@gmail.com>
2017-06-11 21:50:10 -0500

    Added commit post date

--- models/comment/comments.js
+++ models/comment/comments.js
@@ -10,6 +10,10 @@ var commentSchema = new mongoose.Schema(
                 ref: 'User'
             },
             username: String
+        },
+          created:{
+            type: Date,
+            default: Date.now
         }   
     });
 
